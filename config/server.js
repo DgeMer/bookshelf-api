b6483960bb7a35061872d1a4c7c9bbb52f75abb9
Anna Nefodova <ann.nefodova@gmail.com>
2020-12-02 16:57:59 +0200

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed0a91bc22a9324b5782abfddf21bc20'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e79c81b-cb63-48e6-ae73-ac1e73dc5741'
+};
