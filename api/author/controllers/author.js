53074a89b94b9797a3d6923242460763fa824070
Anna Nefodova <ann.nefodova@gmail.com>
2020-12-03 17:14:45 +0200

    customize api; added user model

--- api/author/controllers/author.js
+++ api/author/controllers/author.js
@@ -1,8 +1,17 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities = await strapi.services.author.find({
+      'user.id': ctx.state.user.id,
+    });
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.author }));
+  },
+};
